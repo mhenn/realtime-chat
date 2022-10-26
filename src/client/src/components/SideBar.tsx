c4770a5a6e836544a27c4c19812639ef6a444e4a
Mike Henning <henning@fh-aachen.de>
2022-10-26 23:18:56 +0200

    client: mocked the general design

--- src/client/src/components/SideBar.tsx
+++ src/client/src/components/SideBar.tsx
@@ -1,18 +1,36 @@
 
 export default function SideBar() {
     return (
-        <div className="w-1/6 h-full shadow-md bg-white px-1 absolute">
-            <ul className="relative">
-                <li className="relative">
-                    <a className="flex justify-center items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">Sidenav link 1</a>
-                </li>
-                <li className="relative">
-                    <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">Sidenav link 2</a>
-                </li>
-                <li className="relative">
-                    <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">Sidenav link 2</a>
-                </li>
-            </ul>
+        <div className="flex flex-col bg-slate-700 w-1/6 h-screen px-4 tex-gray-900 border border-purple-900">
+            <div className="flex flex-wrap mt-8">
+                <div className="w-1/2">
+                    <img
+                        src="https://randomuser.me/api/portraits/women/27.jpg"
+                        className="mx-auto w-20 h-20 rounded-full"
+                    />
+                </div>
+                <div className="w-1/2">
+                    <span className="font-semibold text-white">Ava Harper</span>
+                </div>
+            </div>
+            <div className="mt-10 mb-4">
+                <ul className="ml-4">
+                    <li className="mb-2 px-4 py-4 text-gray-100 flex flex-row  border-gray-300 hover:text-black   hover:bg-gray-300  hover:font-bold rounded rounded-lg">
+                        <span>
+                            <svg className="fill-current h-5 w-5 " viewBox="0 0 24 24">
+                                <path
+                                    d="M16 20h4v-4h-4m0-2h4v-4h-4m-6-2h4V4h-4m6
+                        4h4V4h-4m-6 10h4v-4h-4m-6 4h4v-4H4m0 10h4v-4H4m6
+                        4h4v-4h-4M4 8h4V4H4v4z"
+                                ></path>
+                            </svg>
+                        </span>
+                        <a href="#">
+                            <span className="ml-2">Dashboard</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/client/src/pages/ChatPage.tsx
+++ src/client/src/pages/ChatPage.tsx
@@ -3,7 +3,6 @@ import SideBar from '../components/SideBar'
 import { useQuery } from '@tanstack/react-query'
 import { supabase } from '../utils/supabaseClient'
 import useStore from '../utils/state'
-import { JsxElement } from 'typescript';
 
 const userDetailQuery = (id: string) => ({
     queryKey: ["users", id],
@@ -20,23 +19,24 @@ export default function ChatPage() {
         return <span>Loading...</span>
     }
 
-
-    const Switch = (props: JsxElement) => {
-
-        return isLoading ? <Loading /> : { props }
-    }
     return (
-
-
-        <div className="flex flex-col">
+        <div className="flex flex-row">
             <SideBar />
-            <div className="flex flex-col w-5/6 h-screen ml-auto">
-                <div className="w-1/2">
-
+            <div className="flex flex-col w-5/6 h-screen ml-auto justify-between bg-slate-300">
+                <div className="w-full h-full px-3 py-3 ">
+                    <div className="w-full h-full bg-slate-500 rounded-md opacity-30 shadow-sm">
+                    </div>
                 </div>
-                <textarea>
-                </textarea>
+                <div className="flex flex-row px-3 py-3">
+                    <textarea className="w-full mr-2 rounded-md" style={{ resize: 'none' }}>
+                    </textarea>
+                    <button className="rounded-md bg-blue-500 w-8 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mx-auto w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </button>
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
