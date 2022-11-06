4bcf1b7be8d6a0452183308410fee826cb5022f5
Mike Henning <henning@fh-aachen.de>
2022-11-06 12:48:51 +0100

    moving from random supabase use to the full t3 stack

--- src/client/src/pages/AuthPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Auth from '../components/Auth'
-export default function AuthPage() {
-  return (
-    <div className="flex mx-auto min-h-screen ">
-        <Auth/>
-    </div>
-  )
-}
