614d02068a0073ba8c61981b333cda996409cb0f
Mike Henning <henning@fh-aachen.de>
2022-11-19 22:51:27 +0100

    overhauled login

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,16 +1,20 @@
 import { FunctionComponent } from 'react'
 
+
+
 const Background: FunctionComponent = (props: any) => {
     return (
-        <div className="bg-black z-0  h-screen w-screen relative overflow-hidden">
-            <div id='stars'></div>
-            <div id='stars2'></div>
-            <div id='stars3'></div>
-            <div className="z-10 absolute border-2 glow border-white circle-center circle-7xl" />
-            <div className="z-10 absolute border-2 glow border-white bg-black circle-center circle-7xl" />
-            {props.children}
-        </div>
+        <>
+            <div className="bg-stone-900 h-screen w-screen polka">
+                <div className="mx-auto my-auto absolute border-r-2 border-t-2  border-stone-700  circle-7xl circle-center ">
+                </div>
+                <div>
+                    {props.children}
+                </div>
+            </div>
+        </>
     )
 }
+
 
 export default Background
